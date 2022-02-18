3ba8041ca824241bc5f0acfd3a592afb368264e5
yj95228 <kei02119@naver.com>
2022-02-19 01:07:07 +0900

    Edit Home page v2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,29 +2,25 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Stamp } from "../asset/Korean_Voting_Stamp.svg";
 import styles from "./Home.module.css";
 import { Paper } from "@mui/material";
-// import Header from "../components/shared/Header";
+import Typist from "react-typist";
 
 function Home() {
-  const now = new Date();
-  const voteDate = new Date("2022-03-09T00:00:00+0900");
-  const distance = voteDate.getTime() - now.getTime();
-  const dDay = Math.floor(distance / (1000 * 60 * 60 * 24));
-
   return (
     <div className={styles.bg}>
-      {/* <main className={styles.container}>
-        <div className={styles.text}>
-          <h2 className={styles.h2}>제20대 대통령 선거</h2>
-          <h1 className={styles.h1}>D - {dDay}</h1>
-          <hr className={styles.hr} />
-          <p className={styles.p}>설명 어쩌구 저쩌구</p>
-        </div> */}
-      <Paper elevation={3} className={styles.paper}>
+      <div className={styles.box}>
+        <h1 className={styles.h1}>대선 후보 지지율 예측</h1>
+        <hr className={styles.hr} />
+        <Typist avgTypingDelay={100} cursor={{ show: false }}>
+          텍스트로 예측해본 후보별 지지율
+        </Typist>
+        <br />▼
+      </div>
+      <Paper elevation={3} className={styles.linkto}>
         <Link to="/about">
           <Stamp className={styles.stamp} />
         </Link>
+        2022.3.9 투표 꾹 !
       </Paper>
-      {/* </main> */}
     </div>
   );
 }
